4a789cc41379dcd312576c4504b0c174a41c92fa
JGeli <88524940+jGeli@users.noreply.github.com>
2022-03-18 18:43:36 +0800

    My First Commit

---
+++ OneDrive/Desktop/socialMongo-Part2/socialm2/src/dummyData.js
@@ -0,0 +1,65 @@
+export const Users = [
+    {
+        id: 1,
+        profilePicture: "/assets/Person/1.jpg",
+        username: "Marty",
+    },
+    {
+        id: 2,
+        profilePicture: "/assets/Person/2.jpg",
+        username: "Alby",
+    },
+    {
+        id: 3,
+        profilePicture: "/assets/Person/3.jpg",
+        username: "Kurt",
+    },
+    {
+        id: 4,
+        profilePicture: "/assets/Person/4.jpg",
+        username: "Big Boss Jim"
+    }
+];
+
+export const Posts = [
+    {
+        id: 1,
+        desc: "Paglatag",
+        photo: "/assets/Post/1.jpg",
+        date: "3 mins ago",
+        userId: 1,
+        like: 420,
+        comment: 9,
+    },
+
+    {
+        id: 2,
+        desc: "Deym",
+        photo: "/assets/Post/2.jpg",
+        date: "3 mins ago",
+        userId: 2,
+        like: 420,
+        comment: 32,
+    },
+
+    {
+        id: 3,
+        desc: "Shat",
+        photo: "/assets/Post/3.jpg",
+        date: "3 mins ago",
+        userId: 3,
+        like: 420,
+        comment: 3,
+    },
+
+    {
+        id: 4,
+        desc: "Just now: usa nga lalake napakdol",
+        photo: "/assets/Post/4.jpg",
+        date: "3 mins ago",
+        userId: 4,
+        like: 420,
+        comment: 1
+    }
+
+];
